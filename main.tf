f81b5959739d4e1d31c0dab787e16561da14b782
VipinChampak <52660365+VipinChampak@users.noreply.github.com>
2021-10-28 16:43:21 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_CLIENT_ID
+  client_secret   = var.SP_CLIENT_SECRET
+  tenant_id       = var.SP_TENANT_ID
   features {}
 }
 
